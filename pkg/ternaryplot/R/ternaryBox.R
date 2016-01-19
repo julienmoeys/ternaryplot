b3e7e4fbc0b395a4d36c05cf57a33ba1249b354d
Julien MOEYS <julienmoeys@yahoo.fr>
2016-01-19 08:55:17 +0100

    ternaryBox() moved to a separate code file

---
+++ pkg/ternaryplot/R/ternaryBox.R
@@ -0,0 +1,120 @@
+
+# +-------------------------------------------------------------+
+# | Package:    ternaryplot                                     |
+# | Language:   R + roxygen2 inline documentation               |
+# | Author(s):  Julien Moeys <Julien.Moeys@@slu.se>             |
+# | License:    AGPL3, Affero General Public License version 3  |
+# +-------------------------------------------------------------+
+
+# Useful: \code{} \code{\link[]{}} 
+
+
+
+# ternaryBox ====================================================
+
+#'Draw a Box around a ternary plot
+#'
+#'Draw a Box around a ternary plot
+#'
+#'
+#'@seealso
+#'  \code{\link[graphics]{box}}.
+#'
+#'
+#'@param s 
+#'  A \code{\link[ternaryplot]{ternarySystem}} object.
+#'
+#'@param .plot 
+#'  Single logical value. Set to \code{FALSE} if you don't want 
+#'  to plot the graphical element and simply returns them as 
+#'  x-y coordinates (or \code{Spatial*} objects if \code{sp} is 
+#'  set to \code{TRUE} in \code{\link{tpPar}}).
+#'
+#'@param \dots
+#'  Additional parameters passed to \code{\link[graphics]{polygon}}.
+#'  You can for instance set \code{border} for the color of the 
+#'  box outline, \code{col}, for the fill-color of the box, 
+#'  \code{lwd} (outline thickness) or \code{lty} (line-type).
+#'
+#'
+#'@return
+#'  Invisibly returns the graphical element as x-y coordinates or 
+#'  a \code{Spatial*} objects (see \code{.plot}).
+#'
+#'
+#'@rdname ternaryBox-methods
+#'
+#'@export 
+#'
+ternaryBox <- function( s, ... ){ 
+    if( missing( s ) ){ 
+        stop( "'s' is missing. Required for low-level plotting commands (like ternaryBox)" ) 
+    }   
+    
+    UseMethod( "ternaryBox" ) 
+}   
+
+
+
+# INTERNAL. Converts x-y points to SpatialPolygons.
+#   The polygon is closed internally (first value added as last 
+#   value)
+#'@importFrom sp Polygon
+#'@importFrom sp Polygons
+#'@importFrom sp SpatialPolygons
+.xy2SpatialPolygons <- function( xy ){ 
+    if( !identical( xy[1], xy[ nrow( xy ), ] ) ){ 
+        xy <- rbind( xy, xy[1,] ) 
+    }   
+    
+    p <- sp::Polygon( coords = xy )
+    p <- sp::Polygons( srl = list( p ), ID = 1L )
+    p <- sp::SpatialPolygons( Srl = list( p ), pO = 1L ) 
+    
+    return( p ) 
+}   
+
+
+
+#'@rdname ternaryBox-methods
+#'
+#'@method ternaryBox ternarySystem
+#'
+#'@export
+#' 
+ternaryBox.ternarySystem <- function( 
+ s, 
+ .plot = TRUE, 
+ ... 
+){  
+    axis.line.lwd <- getTpPar( "axis.line.lwd" )
+    fg            <- par( "fg" )
+    
+    scale <- s[[ 'scale' ]]
+    
+    ## Convert the scale into a triangular frame
+    tpBox <- data.frame( 
+        "B"    = c( scale[ 1, 1 ], scale[ 2, 1 ], scale[ 1, 1 ] ), 
+        "L"    = c( scale[ 1, 2 ], scale[ 1, 2 ], scale[ 2, 2 ] ), 
+        "R"    = c( scale[ 2, 3 ], scale[ 1, 3 ], scale[ 1, 3 ] ), 
+        "row.names" = c( "left", "right", "top" ) 
+    )   
+    
+    colnames( tpBox ) <- blrNames( s = s )   
+    
+    
+    # Convert the scale to x-y values
+    tpBox <- ternary2xy( s = s, x = tpBox ) 
+    
+    if( .plot ){ 
+        polygon( x = tpBox[, "x" ], y = tpBox[, "y" ], 
+            lwd = axis.line.lwd, border = fg, ... )
+    }   
+    
+    out <- tpBox[, c( "x", "y" ) ] 
+    if( getTpPar( "sp" ) ){ out <- .xy2SpatialPolygons( xy = out ) }
+    
+    return( invisible( out ) ) 
+}   
+
+
