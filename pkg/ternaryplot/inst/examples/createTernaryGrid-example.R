aad9832b87ab8ba609c8d5d23681eb23266f6f70
Julien MOEYS <julienmoeys@yahoo.fr>
2016-01-19 13:18:02 +0100

    fix a bug in createTernaryGrid example

--- pkg/ternaryplot/inst/examples/createTernaryGrid-example.R
+++ pkg/ternaryplot/inst/examples/createTernaryGrid-example.R
@@ -2,17 +2,21 @@
 library( "ternaryplot" ) 
 
 #   Fetch the definition of a ternary system
+#   (which has not classes)
 s <- ternaryPlot( s = "default" ) 
+
 
 #   Create the ternary grid and convert it to 
 #   sp SpatialPolygonsDataFrame
 tg   <- createTernaryGrid( s )
-tgSp <- ternary2SpatialPolygonsDataFrame( tg )
+
 
 #   Plot the ternary system and the grid
 ternaryPlot( s = s )
-ternaryPlot( s = tg, add = FALSE, 
-    polygonExtra = list( border = "red", lty = 2 ) ) 
+ternaryPolygons( s = tg, border = "darkred", lty = 2 ) 
+
 
 # #   Alternatively:
+# ternaryPlot( s = s )
+# tgSp <- ternary2SpatialPolygonsDataFrame( tg ) 
 # sp::plot( tgSp, add = TRUE, border = "darkred", lty = 3 ) 
