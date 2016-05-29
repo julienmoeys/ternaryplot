351f2107b1c5fe7a4fbfcc9abaa98f342d27eeb6
Julien Moeys <julienmoeys@yahoo.fr>
2016-05-29 12:20:05 +0200

    Remove option not necessary

--- ternaryplot_1_RCMDbuild.bat
+++ ternaryplot_1_RCMDbuild.bat
@@ -5,6 +5,6 @@ cd /D "%dropboxPath%\_WORK\_PROJECTS\r_packages\ternaryplot\pkg"
 REM svnversion > %pkgname%\REVISION
 git log -n 1 --oneline --no-notes > %pkgname%\inst\GIT_VERSION
 
-R CMD build --compact-vignettes="gs+qpdf" %pkgname% 
+R CMD build %pkgname% 
 
 pause
