a40866bf4b8d19e128391d227f5b0b2d738094d1
Julien MOEYS <julienmoeys@yahoo.fr>
2016-06-03 17:51:13 +0200

    Edit package description

--- pkg/ternaryplot/R/aa00-ternaryplot-package.R
+++ pkg/ternaryplot/R/aa00-ternaryplot-package.R
@@ -1,31 +1,44 @@
 
-# +-------------------------------------------------------------+
-# | Language: R + roxygen2 inline documentation
-# | Package: ternaryplot 
-# | Author(s): Julien Moeys <Julien.Moeys@@slu.se> 
-# | License: AGPL3, Affero General Public License version 3 
-# +-------------------------------------------------------------+
+# +--------------------------------------------------------+
+# | Language: R + roxygen2 inline documentation            |
+# | Package: ternaryplot                                   |
+# | Author(s): Julien Moeys <Julien.Moeys@@slu.se>         |
+# | License: AGPL3, Affero General Public License v3       |
+# +--------------------------------------------------------+
 
 
 
-#' Ternary plots and ternary classifications.
+#'Infrastructure for ternary plots, ternary classifications and ternary binning (S3-classes).
 #'
-#' Ternary plots and ternary classifications. S4 classes for ternary 
-#'  data, ternary plots and ternary classification.
-#'
-#' @author Julien Moeys \email{Julien.Moeys@@slu.se} 
+#'Infrastructure for ternary plots, ternary 
+#'    classifications and ternary binning (with S3-classes). 
+#'    Designed primarily to support the development of soil 
+#'    texture plots (soil texture diagrams), but should be 
+#'    a useful infrastructure for other ternary plots as well, 
+#'    and especially for ternary classifications. Plots fully 
+#'    compatible with the graphics-package. 'ternaryplot' 
+#'    attempts to respect as much as possible (a) the plot 
+#'    layout of the graphics-package, (b) the size and location 
+#'    of plot-items such as axis tick marks and (c) the name 
+#'    of parameters in the graphics-package. Function names and 
+#'    functions arguments are as much as possible similar to 
+#'    those in the graphics-package. Several ternary plot 
+#'    geometries are supported, but all are based on a single 
+#'    triangle with a horizontal base.
+#'    
+#'@author Julien Moeys \email{Julien.Moeys@@slu.se} 
 #' 
-#' Maintainer: Julien Moeys \email{Julien.Moeys@@slu.se} 
+#'Maintainer: Julien Moeys \email{Julien.Moeys@@slu.se} 
 #'
-#' @docType package
+#'@docType package
 #'
-#' @title Ternary plots and ternary classifications
+#'@title Infrastructure for ternary plots, ternary classifications and ternary binning (S3-classes).
 #'
-#' @keywords package
+#'@keywords package
 #'
-#' @examples
+#'@examples
 #'  # Examples coming later
 #' 
-#' @name ternaryplot-package
+#'@name ternaryplot-package
 #'
 NULL
