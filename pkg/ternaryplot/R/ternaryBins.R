7945991ce4bb7a0a5775897444027b1032dee41c
Julien MOEYS <julienmoeys@yahoo.fr>
2016-06-17 17:56:12 +0200

    Most of the plot part of ternaryBin moved to ternaryPolygons

--- pkg/ternaryplot/R/ternaryBins.R
+++ pkg/ternaryplot/R/ternaryBins.R
@@ -23,7 +23,7 @@
 #'  \code{\link[ternaryplot]{ternaryCount}} for producing 
 #'  the bins and count per bins but not plotting them.
 #'
-#'  Package-options \code{bin.border.col} and \code{bin.bg.fun}  
+#'  Package-options \code{ternaryPolygons.border.col} and \code{ternaryPolygons.bg.fun}  
 #'  in \code{\link[ternaryplot]{tpPar}} are used internally.
 #'
 #'
@@ -49,43 +49,6 @@
 #'  to \code{FALSE} to count the number of \code{x}-data 
 #'  points per class in \code{s} instead.
 #'
-#'@param breaks 
-#'  See \code{\link[base]{cut}}. Break-values or number of 
-#'  groups to be used when preparing the colour-scale and 
-#'  the colour-scale legend for the binned values. See 
-#'  also \code{cut.args} below.
-#'
-#'@param cut.args 
-#'  If not \code{NULL}, a list of additional arguments to be 
-#'  passed to \code{\link[base]{cut}} (see \code{breaks} 
-#'  above). Values \code{x}, \code{breaks}, \code{dig.lab} 
-#'  and \code{include.lowest} not allowed. \code{dig.lab} 
-#'  is internally set to 0 (as counts are integer values) 
-#'  and \code{include.lowest} is set to \code{TRUE} internally.
-#'
-#'@param legend.add 
-#'  Single logical value. If \code{TRUE} (the default), 
-#'  a legend is added on the plot.
-#'
-#'@param legend.x 
-#'  See argument \code{x} in \code{\link[graphics]{legend}}. 
-#'  Position of the legend. Default value is \code{topright} 
-#'  unless \code{tlrAngles(s)[3L]} is higher than 60 degrees 
-#'  (which indicates a ternary system with a right-angle 
-#'  located at the right side of the plot), in which case 
-#'  the value is set to \code{topleft} (set internally in 
-#'  all cases).
-#'
-#'@param legend.title 
-#'  See argument \code{title} in \code{\link[graphics]{legend}}. 
-#'  Title of the legend.
-#'
-#'@param legend.args 
-#'  If not \code{NULL}, a list of additional arguments to be 
-#'  passed to \code{\link[graphics]{legend}}. Arguments 
-#'  \code{x} and \code{title} are excluded and should be set 
-#'  with \code{legend.x} and \code{legend.title}.
-#'
 #'@param noZero 
 #'  Single logical value. If \code{TRUE} (the default), 
 #'  zero-counts values are not shown on the plot. This also 
@@ -93,7 +56,8 @@
 #'
 #'@param \dots 
 #'  Additional parameters passed to 
-#'  \code{\link[ternaryplot]{ternaryPolygons}}.
+#'  \code{\link[ternaryplot]{ternaryPolygons}}. See in particular 
+#'  
 #'
 #'
 #'@return 
@@ -135,12 +99,6 @@ ternaryBins.ternarySystem <- function(
     s, 
     x, 
     grid = TRUE, 
-    breaks = 6L, 
-    cut.args = NULL, 
-    legend.add = TRUE, 
-    legend.x = NULL, 
-    legend.title = "counts", 
-    legend.args = NULL, 
     noZero = TRUE, 
     ... 
 ){  
@@ -149,78 +107,9 @@ ternaryBins.ternarySystem <- function(
     grd <- ternaryCount.ternarySystem( s = s, x = x, 
         grid = grid )
     
-    counts <- attr( x = grd, which = "counts" )
     
-    if( noZero ){
-        idCol <- attr( x = grd, which = "idCol" ) 
-        
-        counts <- counts[ counts > 0 ]
-        
-        grd <- grd[ grd[, idCol ] %in% names( counts ), ]
-        
-        attr( x = grd, which = "counts" ) <- counts 
-    }   
+    ternaryPolygons( s = grd, z = "counts", noZero = noZero, ... )   
     
-    if( length( breaks ) == 1L ){
-        cRange <- range( counts, na.rm = TRUE ) 
-        breaks <- round( seq( 
-            from       = cRange[ 1L ],
-            to         = cRange[ 2L ],
-            length.out = breaks ), digits = 0L ) 
-        breaks <- unique( breaks ) 
-    }   
-    
-    cut.args0 <- list( 
-        "x"              = counts, 
-        "breaks"         = breaks, 
-        "dig.lab"        = 0L, 
-        "include.lowest" = TRUE 
-    )   
-    
-    if( !is.null( cut.args ) ){
-        if( !("list" %in% class( cut.args )) ){
-            stop( "If not NULL 'cut.args' must be a list" )
-        }   
-        
-        cut.args0 <- c( cut.args0, cut.args ) 
-    }   
-    
-    ccounts <- do.call( what = "cut", args = cut.args0 )
-    
-    bin.bg.fun <- .tpPar[[ "bin.bg.fun" ]]
-    
-    .levels <- levels( ccounts ) 
-    bg0     <- rev( bin.bg.fun( n = length( .levels ) ) ) 
-    names( bg0 ) <- .levels 
-    bg      <- bg0[ as.character( ccounts ) ]
-    
-    border <- .tpPar[[ "bin.border.col" ]] 
-    
-    ternaryPolygons( s = grd, bg = as.character( bg ), 
-        border = border, ... )   
-    
-    if( legend.add ){
-        if( is.null( legend.x ) ){
-            if( tlrAngles( s )[ 3L ] <= 60 ){
-                legend.x <- "topright"
-            }else{
-                legend.x <- "topleft"
-            }   
-        }   
-        
-        legend.args0 <- list( 
-            "x"       = legend.x, 
-            "legend"  = .levels, 
-            "fill"    = bg0, 
-            "title"   = legend.title 
-        )   
-        
-        if( !is.null( legend.args ) ){
-            legend.args0 <- c( legend.args0, legend.args ) 
-        }   
-        
-        do.call( what = "legend", args = legend.args0 )
-    }   
     
     return( invisible( grd ) ) 
 }   
